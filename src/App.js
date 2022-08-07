e44bd4eb23b0eddd146ad08f3e57bd90e00a3b68
talhaunas1 <mtalha4600@gmail.com>
2022-08-07 13:03:04 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './component/header/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Header/>
+
+
+    {/* card */}
+
+
+    {/* swipebutton */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+import PersonIcon from '@mui/icons-material/Person';
+
+
+function Header() {
+  return (
+    <div>
+        <h2>I am a Header</h2>
+    </div>
+  )
+}
+
+export default Header
