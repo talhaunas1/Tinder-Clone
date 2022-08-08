bdbcd3886570536e1ac8ea65e14f2fc6d420ecf0
talhaunas1 <mtalha4600@gmail.com>
2022-08-08 20:01:32 +0500

    styling footer

--- src/component/footer/SwipeButtons.js
+++ src/component/footer/SwipeButtons.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import './SwipeButtons.css'
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import { IconButton } from '@mui/material';
+
 function SwipeButtons() {
   return (
-    <div className='SwipeButtons'>SwipeButtons</div>
+    <div className='SwipeButtons'>
+
+        <IconButton className='btn_replay MuiIconButtin-root'>
+            <ReplayIcon fontSize='large'/>
+        </IconButton>
+
+        <IconButton className='btn_close MuiIconButtin-root'>
+            <CloseIcon fontSize='large'/>
+        </IconButton>
+        
+        <IconButton className='btn_star MuiIconButtin-root'>
+            <StarRateIcon fontSize='large'/>
+        </IconButton>
+        
+        <IconButton className='btn_fvrt MuiIconButtin-root'>
+            <FavoriteIcon fontSize='large'/>
+        </IconButton>
+
+        <IconButton className='btn_flash MuiIconButtin-root'>
+            <FlashOnIcon fontSize='large'/>
+        </IconButton>
+
+    </div>
   )
 }
 
